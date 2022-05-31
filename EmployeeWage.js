7bfd832c3b124c6af598da15a2c8e1150a971087
SwatiBadole <swatibadole1997@gmail.com>
2022-05-30 23:19:09 -0700

    Added:Refactor with the function

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,18 +1,27 @@
-//UC2:Ability to Calculate Daily Employee Wage based on part time or full time work
-const IS_PART_TIME=1;
-const IS_FULL_TIME=2;
-const PART_TIME_HOURS=4;
-const FULL_TIME_HOURS=8;
-const WAGE_PER_HOUR=20;
+console.log("Welcome to Employee wage program.");
 
-let empHrs=0;
-empCheck =Math.floor(Math.random() * 10) % 3;
-switch (empCheck){
-    case IS_PART_TIME :empHrs =PART_TIME_HOURS;
-    break;
-    case IS_FULL_TIME: empHrs = FULL_TIME_HOURS;
-    default:
-        empHrs =0;
+//UC 3
+const IS_ABSENT = 0;
+const IS_FULL_TIME = 1;
+const IS_PART_TIME = 2;
+const FULL_TIME_HOURS = 8;
+const PART_TIME_HOURS = 4;
+const WAGE_PER_HOUR = 20;
+
+function getWorkingHours(empCheck){
+    switch (empCheck){
+        case IS_ABSENT:
+            return 0;
+        
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+    }
 }
-let empWage = empHrs *WAGE_PER_HOUR;
-console.log("Employee wage : "+ empWage);
+let empHours = 0;
+let empCheck = Math.floor(Math.random()*10)%3;
+empHours = getWorkingHours(empCheck);
+let empWage = empHours * WAGE_PER_HOUR;
+console.log("Employee wage: "+ empWage);
