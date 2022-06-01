9217120354fedf79b70154487d3de9589d7f3218
SwatiBadole <swatibadole1997@gmail.com>
2022-05-31 17:12:01 -0700

    Added:calculate total working days,total hours,employee wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,22 +1,59 @@
 
-//UC6:Storing Daily Wage in a array
-function calcDailyWage(empHrs){
-    return empHrs * WAGE_PER_HOUR;
-}
-const MAX_HRS_IN_MONTH =160;
-const NUM_OF_WORKING_DAYS= 20;
-let totalEmpHrs=0;
-let totalWorkingDays=0;
-let empDailyWageArr =new Array();
+console.log("Welcome to Employee wage program.");
 
-while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+//UC 
+const IS_ABSENT = 0;
+const IS_FULL_TIME = 1;
+const IS_PART_TIME = 2;
+const FULL_TIME_HOURS = 8;
+const PART_TIME_HOURS = 4;
+const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
+const MAX_DAYS_IN_MONTH = 20;
+
+function getWorkingHours(empCheck){
+    switch (empCheck){
+        case IS_ABSENT:
+            return 0;
+        
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+    }
+}
+let empHours = 0;
+let totalWorkingHours = 0;
+let totalWorkingDays = 0;
+let empDailyWageArray = new Array();
+
+function calculateDailyWage(empHours){
+    return empHours * WAGE_PER_HOUR;
+}
+
+while (totalWorkingDays < MAX_DAYS_IN_MONTH && totalWorkingHours < MAX_HRS_IN_MONTH) {
     totalWorkingDays++;
-    let empCheck =Math.floor(Math.random() * 10) % 3;
-    let empHrs = gerWorkingHours(empCheck);
-    totalEmpHrs += empHrs;
-    empDailyWageArr.push(calcDailyWage(empHrs));
+    let empCheck = Math.floor(Math.random()*10)%3;
+    empHours = getWorkingHours(empCheck);
+    totalWorkingHours = totalWorkingHours + empHours;
+    empDailyWageArray.push(calculateDailyWage(empHours))  //Storing daily wage in array.
 }
-let empWage = calcDailyWage(totalEmpHrs);
-console.log("UC6-total days :" +totalWorkingDays+"total Hrs:"+totalEmpHrs+"Emp Wage: "+empWage);
+let empWage = calculateDailyWage(totalWorkingHours);
+console.log("Total Days: "+totalWorkingDays +" Total Hours: "+totalWorkingHours+ " Employee wage: "+ empWage);
 
-//output:Total Days: 20 Total Hours: 84 Employee wage: 1680
+//UC 7A
+let totEmpWage = 0;
+
+function sum(dailyWage){
+    totEmpWage += dailyWage;
+}
+empDailyWageArray.forEach(sum);
+console.log("UC7 A >> Total Days: "+totalWorkingDays +" Total Hours: "+totalWorkingHours+ " Employee wage: "+ empWage);
+
+function totalWages(totalWage, dailyWage){
+    return totalWage + dailyWage;
+}
+
+console.log("UC7 A >> Employee Wage with reduce: "+ empDailyWageArray.reduce(totalWages,0));
